cc1d36484ba61b5c81f418f24cba4fbe2fced30c
UyCode <Hyper-Hack@outlook.com>
2021-03-12 11:31:30 +0800

    server added

---
+++ src/main/java/com/uycode/mockapiserver/MockApiServerApplication.java
@@ -0,0 +1,16 @@
+package com.uycode.mockapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hyper
+ */
+@SpringBootApplication
+public class MockApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uycode/mockapiserver/MockApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uycode.mockapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
